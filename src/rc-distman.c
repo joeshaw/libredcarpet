f99f4f87726e69b31620da9eeafda317d3054521
Joe Shaw <joe@helixcode.com>
2000-07-25 07:34:25 +0000

    Filled out. It takes a pointer to a list of packages and depending on the
    2000-07-25  Joe Shaw  <joe@helixcode.com>
    
    	* src/gui.c (checkbox_toggled): Filled out. It takes a pointer to a
    	list of packages and depending on the checkbox status, appends or
    	removes from the list. It also sensitizes the install button depending
    	on whether the list is empty.
    	(select_all_available): Callback for a convenience button. Selects all
    	available packages. It is passed an integer that represents the total
    	number of buttons. It then extracts the checkbox widgets based on that
    	info and sets them on.
    	(unselect_all_available): Callback for a convenience button. Unselects
    	all available packages. Works the same way as select_all_available.
    	(install_available_packages): Callback for the install button.
    	Basically calls gui_install_packages directly.
    	(render_available_html): Added creation and signal handling for the
    	buttons for selecting and unselecting all packages and installing.
    
    	* src/rc-distman.c (rc_distman_new): Now checks an environment
    	variable, RC_PACKMAN_TYPE which can be either "dpkg" or "rpm" and
    	will override the default setting for creating a packman object.

--- src/rc-distman.c
+++ src/rc-distman.c
@@ -23,24 +23,32 @@ rc_distman_new (void)
 {
     DistributionInfo distro;
 	RCPackman *packman = NULL;
+    char *env;
 
     rc_determine_distro (&distro);
 
-    switch (distro.type) {
-	case DISTRO_UNSUPPORTED:
-	case DISTRO_UNKNOWN:
+    env = getenv("RC_PACKMAN_TYPE");
+    if (env && g_strcasecmp(env, "dpkg") == 0)
+        packman = RC_PACKMAN(rc_debman_new());
+    else if (env && g_strcasecmp(env, "rpm") == 0)
+        packman = RC_PACKMAN(rc_rpmman_new());
+    else {
+        switch (distro.type) {
+        case DISTRO_UNSUPPORTED:
+        case DISTRO_UNKNOWN:
+            break;
+        case DISTRO_DEBIAN:
+        case DISTRO_COREL:
+            packman = RC_PACKMAN (rc_debman_new ());
         break;
-    case DISTRO_DEBIAN:
-	case DISTRO_COREL:
-        packman = RC_PACKMAN (rc_debman_new ());
-        break;
-    default:
+        default:
 #ifdef HAVE_LIBRPM
-        packman = RC_PACKMAN (rc_rpmman_new ());
+            packman = RC_PACKMAN (rc_rpmman_new ());
 #else
-        g_warning ("RPM support not enabled.");
+            g_warning ("RPM support not enabled.");
 #endif
-        break;
+            break;
+        }
     }
 
     return packman;
