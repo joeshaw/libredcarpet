73376bf5ec161818a5928f0ce9b8fe02f54bd5c0
Ian Peters <itp@flux.helixcode.com>
2000-08-22 21:36:12 +0000

    some code I grabbed out of dpkg to do md5sum of files, altered to use
    2000-08-22  Ian Peters  <itp@flux.helixcode.com>
    
            * src/rc-md5.c, src/rc-md5.h, src/rc-md5-private.h: some code I grabbed
            out of dpkg to do md5sum of files, altered to use standard glib types.
            This doesn't handle endianness issues (the code is there but we need to
            check for that in configure), but works.  The only thing you should
            include is rc-md5.h, and all you should need to do is
            rc_md5(filename)
    
            * src/Makefile.am: add rc-md5* to the libredcarpet sources
    
            * src/common.h: add an md5sum field to the RCPackageUpdate struct,
            which contains the md5 sum as calculated by the rc_md5 function of the
            package in question.
    
            * src/common.c: free the md5sum when we free the RCPackageUpdate
    
            * src/pkginfo.c: get the md5sum out of the xml file
    
            * src/make_pkgs.c: calculate and add the md5sum to the generated xml
            file
    
            * src/add_update.c: calculate and add the md5sum to the xml file
    
            Unfortunately, nothing is yet done on download and install /with/ the
            md5 sums we're now calculating, but that remains for the massive rework
            of the gui_install stuff.

---
+++ src/rc-md5.h
@@ -0,0 +1,10 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef _RC_MD5
+#define _RC_MD5
+
+#include <glib.h>
+
+gchar *rc_md5 (const gchar *filename);
+
+#endif /* RC_MD5 */
