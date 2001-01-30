87f861be81920e8a9eb3d74e8f4ebf807818fb24
Ian Peters <itp@src.gnome.org>
2001-01-30 07:48:50 +0000

    try again
    * rc-rpmman.c (vercmp): try again

--- src/rc-rpmman.c
+++ src/rc-rpmman.c
@@ -1524,83 +1524,74 @@ vercmp(const char * a, const char * b)
     /* easy comparison to see if versions are identical */
     if (!strcmp(a, b)) return 0;
 
-    /* The alloca's were silly. The loop below goes through pains
-       to ensure that the old character is restored; this just means
-       that we have to make sure that we're not passing in any
-       static strings. I think we're OK with that.
-    */
-    /* I'm not ok with it.  At least for now, it's really a pain to
-     * get random SEGV's when we pass const strings.  Defensive
-     * programming and all. */
+    str1 = alloca(strlen(a) + 1);
+    str2 = alloca(strlen(b) + 1);
 
-    str1 = alloca (strlen (a) + 1);
-    str2 = alloca (strlen (b) + 1);
-
-    strcpy (str1, a);
-    strcpy (str2, b);
+    strcpy(str1, a);
+    strcpy(str2, b);
 
     one = str1;
     two = str2;
 
     /* loop through each version segment of str1 and str2 and compare them */
     while (*one && *two) {
-        while (*one && !isalnum(*one)) one++;
-        while (*two && !isalnum(*two)) two++;
+	while (*one && !isalnum(*one)) one++;
+	while (*two && !isalnum(*two)) two++;
 
-        str1 = one;
-        str2 = two;
+	str1 = one;
+	str2 = two;
 
-        /* grab first completely alpha or completely numeric segment */
-        /* leave one and two pointing to the start of the alpha or numeric */
-        /* segment and walk str1 and str2 to end of segment */
-        if (isdigit(*str1)) {
-            while (*str1 && isdigit(*str1)) str1++;
-            while (*str2 && isdigit(*str2)) str2++;
-            isnum = 1;
-        } else {
-            while (*str1 && isalnum(*str1)) str1++;
-            while (*str2 && isalnum(*str2)) str2++;
-            isnum = 0;
-        }
+	/* grab first completely alpha or completely numeric segment */
+	/* leave one and two pointing to the start of the alpha or numeric */
+	/* segment and walk str1 and str2 to end of segment */
+	if (isdigit(*str1)) {
+	    while (*str1 && isdigit(*str1)) str1++;
+	    while (*str2 && isdigit(*str2)) str2++;
+	    isnum = 1;
+	} else {
+	    while (*str1 && isalpha(*str1)) str1++;
+	    while (*str2 && isalpha(*str2)) str2++;
+	    isnum = 0;
+	}
 
-        /* save character at the end of the alpha or numeric segment */
-        /* so that they can be restored after the comparison */
-        oldch1 = *str1;
-        *str1 = '\0';
-        oldch2 = *str2;
-        *str2 = '\0';
+	/* save character at the end of the alpha or numeric segment */
+	/* so that they can be restored after the comparison */
+	oldch1 = *str1;
+	*str1 = '\0';
+	oldch2 = *str2;
+	*str2 = '\0';
 
-        /* take care of the case where the two version segments are */
-        /* different types: one numeric and one alpha */
-        if (one == str1) return -1;     /* arbitrary */
-        if (two == str2) return -1;
+	/* take care of the case where the two version segments are */
+	/* different types: one numeric and one alpha */
+	if (one == str1) return -1;	/* arbitrary */
+	if (two == str2) return -1;
 
-        if (isnum) {
-            /* this used to be done by converting the digit segments */
-            /* to ints using atoi() - it's changed because long  */
-            /* digit segments can overflow an int - this should fix that. */
+	if (isnum) {
+	    /* this used to be done by converting the digit segments */
+	    /* to ints using atoi() - it's changed because long  */
+	    /* digit segments can overflow an int - this should fix that. */
 
-            /* throw away any leading zeros - it's a number, right? */
-            while (*one == '0') one++;
-            while (*two == '0') two++;
+	    /* throw away any leading zeros - it's a number, right? */
+	    while (*one == '0') one++;
+	    while (*two == '0') two++;
 
-            /* whichever number has more digits wins */
-            if (strlen(one) > strlen(two)) return 1;
-            if (strlen(two) > strlen(one)) return -1;
-        }
+	    /* whichever number has more digits wins */
+	    if (strlen(one) > strlen(two)) return 1;
+	    if (strlen(two) > strlen(one)) return -1;
+	}
 
-        /* strcmp will return which one is greater - even if the two */
-        /* segments are alpha or if they are numeric.  don't return  */
-        /* if they are equal because there might be more segments to */
-        /* compare */
-        rc = strcmp(one, two);
-        if (rc) return rc;
+	/* strcmp will return which one is greater - even if the two */
+	/* segments are alpha or if they are numeric.  don't return  */
+	/* if they are equal because there might be more segments to */
+	/* compare */
+	rc = strcmp(one, two);
+	if (rc) return rc;
 
-        /* restore character that was replaced by null above */
-        *str1 = oldch1;
-        one = str1;
-        *str2 = oldch2;
-        two = str2;
+	/* restore character that was replaced by null above */
+	*str1 = oldch1;
+	one = str1;
+	*str2 = oldch2;
+	two = str2;
     }
 
     /* this catches the case where all numeric and alpha segments have */
