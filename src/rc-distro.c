4b34c757c7085fda28e8a86052fcfa1944a10ad4
Ian Peters <itp@src.gnome.org>
2001-08-01 20:54:03 +0000

    s/sun4/sun4u/
    * rc-distro.c: s/sun4/sun4u/

--- src/rc-distro.c
+++ src/rc-distro.c
@@ -143,11 +143,11 @@ RCDistroChunk distro_figurers[] = {
     { "debian-woody-i386", RC_ARCH_IA32,
       func_string_in_file, "/etc/debian_version", "testing", NULL, 0},
 
-    { "solaris-7-sun4", RC_ARCH_SPARC,
+    { "solaris-7-sun4u", RC_ARCH_SPARC,
       func_sys, "uname -s", "SunOS", (gpointer) 0, CHECK_OP_AND,
       func_sys, "uname -r", "5.7", (gpointer) 0 },
 
-    { "solaris-8-sun4", RC_ARCH_SPARC,
+    { "solaris-8-sun4u", RC_ARCH_SPARC,
       func_sys, "uname -s", "SunOS", (gpointer) 0, CHECK_OP_AND,
       func_sys, "uname -r", "5.8", (gpointer) 0 },
 
@@ -186,8 +186,8 @@ RCDistroType distro_types[] = {
     { "yellowdog-12-ppc", NULL, "Yellow Dog Linux", "1.2", RC_PKG_RPM, RC_ARCH_PPC, "gdm-runlevel=5, non-gdm-runlevel=3" },
     { "yellowdog-20-ppc", NULL, "Yellow Dog Linux", "2.0", RC_PKG_RPM, RC_ARCH_PPC, "gdm-runlevel=5, non-gdm-runlevel=3" },
 
-    { "solaris-7-sun4", NULL, "Sun Solaris 7", "7", RC_PKG_RPM, RC_ARCH_SPARC, "" },
-    { "solaris-8-sun4", NULL, "Sun Solaris 8", "8", RC_PKG_RPM, RC_ARCH_SPARC, "" },
+    { "solaris-7-sun4u", NULL, "Sun Solaris 7", "7", RC_PKG_RPM, RC_ARCH_SPARC, "" },
+    { "solaris-8-sun4u", NULL, "Sun Solaris 8", "8", RC_PKG_RPM, RC_ARCH_SPARC, "" },
 
     { NULL }
 };
@@ -345,7 +345,7 @@ determine_arch ()
             ret = RC_ARCH_IA32;
         }
 
-        if (!strncmp (buf, "sun4", 4) ||
+        if (!strncmp (buf, "sun4u", 5) ||
             !strncmp (buf, "sparc", 5))
         {
             ret = RC_ARCH_SPARC;
