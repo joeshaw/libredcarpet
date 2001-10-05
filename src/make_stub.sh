7a3d6f0e691b2d8370ea811f6f8a780d3255e011
Ian Peters <itp@src.gnome.org>
2001-10-05 00:12:53 +0000

    cosmetic fix

--- src/make_stub.sh
+++ src/make_stub.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -f libstub-*.so
+myid=0
+
 stub=$1
 shift
 
@@ -7,7 +10,8 @@ line="ld -L. -shared -o $stub"
 
 while [ $# -gt 0 ]
 do
-    lib=`mktemp -u XXXXXXXX`
+    myid=`expr $myid + 1`
+    lib=stub-$myid
     ld -soname $1 -shared -o lib$lib.so -lc
     remove="$remove lib$lib.so"
     line="$line -l$lib"
