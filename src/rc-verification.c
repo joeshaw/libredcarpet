51e82955f65e86fdaf457240e4cfd5b89a39b4fe
Ian Peters <itp@src.gnome.org>
2002-08-07 17:35:05 +0000

    refactored to use g_spawn instead of handling it myself. Other
    * rc-verification.c (rc_verify_gpg): refactored to use g_spawn
    instead of handling it myself.  Other miscellaneous cleanups, too.

--- src/rc-verification.c
+++ src/rc-verification.c
@@ -50,7 +50,7 @@ rc_verification_new ()
     new = g_new0 (RCVerification, 1);
 
     return (new);
-} /* rc_verification_new */
+}
 
 void
 rc_verification_free (RCVerification *verification)
@@ -58,7 +58,7 @@ rc_verification_free (RCVerification *verification)
     g_free (verification->info);
 
     g_free (verification);
-} /* rc_verification_free */
+}
 
 void
 rc_verification_slist_free (RCVerificationSList *verification_list)
@@ -66,23 +66,12 @@ rc_verification_slist_free (RCVerificationSList *verification_list)
     g_slist_foreach (verification_list, (GFunc) rc_verification_free, NULL);
 
     g_slist_free (verification_list);
-} /* rc_verification_slist_free */
-
-typedef struct _GPGInfo GPGInfo;
-
-struct _GPGInfo {
-    GMainLoop *loop;
-
-    RCVerification *verification;
-
-    guint read_line_id;
-    guint read_done_id;
-};
+}
 
 static void
 gpg_read_line_cb (RCLineBuf *line_buf, gchar *line, gpointer data)
 {
-    GPGInfo *gpg_info = (GPGInfo *)data;
+    RCVerification *verification = data;
     const gchar *ptr;
 
     rc_debug (RC_DEBUG_LEVEL_DEBUG, __FUNCTION__ ": got \"%s\"\n", line);
@@ -92,14 +81,14 @@ gpg_read_line_cb (RCLineBuf *line_buf, gchar *line, gpointer data)
     if (!strncmp (ptr, "GOODSIG ", strlen ("GOODSIG "))) {
         rc_debug (RC_DEBUG_LEVEL_INFO, __FUNCTION__ ": good GPG signature\n");
 
-        gpg_info->verification->status = RC_VERIFICATION_STATUS_PASS;
+        verification->status = RC_VERIFICATION_STATUS_PASS;
 
         ptr = strstr (ptr + strlen ("GOODSIG "), " ");
 
         if (ptr) {
             rc_debug (RC_DEBUG_LEVEL_INFO, __FUNCTION__ \
                       ": signer is \"%s\"\n", ptr + 1);
-            gpg_info->verification->info = g_strdup (ptr + 1);
+            verification->info = g_strdup (ptr + 1);
         }
 
         return;
@@ -108,14 +97,14 @@ gpg_read_line_cb (RCLineBuf *line_buf, gchar *line, gpointer data)
     if (!strncmp (ptr, "BADSIG ", strlen ("BADSIG "))) {
         rc_debug (RC_DEBUG_LEVEL_INFO, __FUNCTION__ ": bad GPG signature\n");
 
-        gpg_info->verification->status = RC_VERIFICATION_STATUS_FAIL;
+        verification->status = RC_VERIFICATION_STATUS_FAIL;
 
         ptr = strstr (ptr + strlen ("BADSIG "), " ");
 
         if (ptr) {
             rc_debug (RC_DEBUG_LEVEL_INFO, __FUNCTION__ \
                       ": signer is \"%s\"\n", ptr + 1);
-            gpg_info->verification->info = g_strdup (ptr + 1);
+            verification->info = g_strdup (ptr + 1);
         }
 
         return;
@@ -125,52 +114,60 @@ gpg_read_line_cb (RCLineBuf *line_buf, gchar *line, gpointer data)
         rc_debug (RC_DEBUG_LEVEL_WARNING, __FUNCTION__ \
                   ": error checking GPG signature\n");
 
-        gpg_info->verification->status = RC_VERIFICATION_STATUS_UNDEF;
+        verification->status = RC_VERIFICATION_STATUS_UNDEF;
 
         return;
     }
-} /* gpg_read_line_cb */
+}
 
 static void
 gpg_read_done_cb (RCLineBuf *line_buf, RCLineBufStatus status, gpointer data)
 {
-    GPGInfo *gpg_info = (GPGInfo *)data;
+    GMainLoop *loop = data;
 
-    g_signal_handler_disconnect (line_buf, gpg_info->read_line_id);
-    g_signal_handler_disconnect (line_buf, gpg_info->read_done_id);
+    g_main_quit (loop);
+}
 
-    g_main_quit (gpg_info->loop);
-} /* gpg_read_done_cb */
+static void
+child_setup_func (gpointer user_data)
+{
+    dup2 (STDOUT_FILENO, STDERR_FILENO);
+}
 
 RCVerification *
 rc_verify_gpg (gchar *file, gchar *sig)
 {
-    pid_t child;
-    int status;
-    int fds[2];
-    GPGInfo gpg_info;
-    RCLineBuf *line_buf;
-    GMainLoop *loop;
-    gchar *gpg_command;
     RCVerification *verification;
-    gchar *gpgdir;
+    static gchar *gpg_command = NULL;
+    static gchar *gpgdir = NULL;
+    static char *list_argv[] =
+        { "%%GPG%%", "--list-keys", NULL };
 
     verification = rc_verification_new ();
 
     verification->type = RC_VERIFICATION_TYPE_GPG;
     verification->status = RC_VERIFICATION_STATUS_UNDEF;
 
-    if (!keyring)
+    /* Our calling application must provide us with a gpg keyring
+     * before we can verify any signatures */
+    if (!keyring) {
+        verification->info = g_strdup ("no gpg keyring was provided");
         return verification;
+    }
 
-    gpg_command = rc_is_program_in_path ("gpg");
-
+    /* If we haven't found gpg in the path yet, we'd better look for
+     * it */
     if (!gpg_command) {
-        verification->status = RC_VERIFICATION_STATUS_UNDEF;
-        verification->info =
-            g_strdup ("gpg does not appear to be on your PATH");
+        gpg_command = rc_is_program_in_path ("gpg");
 
-        return (verification);
+        /* Without gpg, we can't verify these signatures at all */
+        if (!gpg_command) {
+            verification->info =
+                g_strdup ("gpg does not appear to be in your PATH");
+            return verification;
+        }
+
+        list_argv[0] = gpg_command;
     }
 
     /* There's a chance that the ~root/.gnupg directory doesn't exist
@@ -183,123 +180,70 @@ rc_verify_gpg (gchar *file, gchar *sig)
      * the off chance that we're wrong, as gpg by itself just blocks
      * waiting for input in normal operation. */
 
-    gpgdir = g_strconcat (g_get_home_dir (), "/.gnupg", NULL);
+    if (!gpgdir)
+        gpgdir = g_strconcat (g_get_home_dir (), "/.gnupg", NULL);
 
     if (!rc_file_exists (gpgdir)) {
-        gchar *command;
-        int result;
-
-        command = g_strconcat (gpg_command, " --list-keys", NULL);
-        result = system (command);
-        g_free (command);
+        /* We don't care about errors, or anything, really, since
+         * we're just going to check for the directory again
+         * afterwards */
+        g_spawn_sync (NULL, list_argv, NULL, 0, NULL, NULL, NULL, NULL,
+                      NULL, NULL);
 
         if (!rc_file_exists (gpgdir)) {
             verification->status = RC_VERIFICATION_STATUS_UNDEF;
             verification->info = g_strdup (
                 "gpg was unable to create ~/.gnupg");
 
-            g_free (gpgdir);
+            return verification;
+        }
+    }
 
-            return (verification);
+    { /* I suppose this could be its own function, but... */
+        RCLineBuf *line_buf;
+        GMainLoop *loop;
+        GError *error;
+        gint stdout_fd;
+        char *verify_argv[] =
+            { gpg_command, "--batch", "--quiet", "--no-secmem-warning",
+              "--no-default-keyring", "--keyring", keyring,
+              "--status-fd", "1", "--logger-fd", "1", "--verify",
+              sig, file, NULL };
+
+        error = NULL;
+        if (!g_spawn_async_with_pipes (
+                NULL, verify_argv, NULL, 0, child_setup_func,
+                NULL, NULL, NULL, &stdout_fd, NULL, &error))
+        {
+            rc_debug (RC_DEBUG_LEVEL_ERROR,
+                      "g_spawn failed: %s", error->message);
+            verification->status = RC_VERIFICATION_STATUS_UNDEF;
+            verification->info = g_strconcat (
+                "unable to verify signature: ", error->message, NULL);
+
+            g_error_free (error);
+
+            return verification;
         }
 
-        g_free (gpgdir);
+        line_buf = rc_line_buf_new ();
+        rc_line_buf_set_fd (line_buf, stdout_fd);
+
+        loop = g_main_new (FALSE);
+
+        g_signal_connect (line_buf, "read_line", (GCallback) gpg_read_line_cb,
+                          verification);
+        g_signal_connect (line_buf, "read_done", (GCallback) gpg_read_done_cb,
+                          loop);
+
+        g_main_run (loop);
+
+        g_object_unref (line_buf);
+        g_main_destroy (loop);
+
+        return verification;
     }
-
-    if (pipe (fds)) {
-        verification->status = RC_VERIFICATION_STATUS_UNDEF;
-        verification->info =
-            g_strdup ("unable to create a pipe to communicate with gpg");
-
-        return (verification);
-    }
-
-    fcntl (fds[0], F_SETFL, O_NONBLOCK);
-    fcntl (fds[1], F_SETFL, O_NONBLOCK);
-
-    signal (SIGCHLD, SIG_DFL);
-
-    child = fork ();
-
-    switch (child) {
-    case -1:
-        rc_debug (RC_DEBUG_LEVEL_ERROR, __FUNCTION__ ": fork() failed\n");
-
-        close (fds[0]);
-        close (fds[1]);
-
-        verification->status = RC_VERIFICATION_STATUS_UNDEF;
-        verification->info =
-            g_strdup ("unable to exec gpg to verify signature");
-
-        return (verification);
-
-    case 0:
-        close (fds[0]);
-
-        fflush (stdout);
-        dup2 (fds[1], STDOUT_FILENO);
-
-        close (fds[1]);
-
-        /* I wish I could add --no-auto-key-retrieve, but it's not in
-         * gpg 1.0.1, and is in 1.0.4.  Maybe we can require a
-         * specific version of gpg... */
-        execl (gpg_command, gpg_command, "--batch", "--quiet",
-               "--no-secmem-warning", "--no-default-keyring",
-               "--keyring", keyring, "--status-fd",
-               "1", "--logger-fd", "1", "--verify", sig, file, NULL);
-
-        _exit (-1);
-
-    default:
-        break;
-    }
-
-    close (fds[1]);
-
-    line_buf = rc_line_buf_new ();
-    rc_line_buf_set_fd (line_buf, fds[0]);
-
-    loop = g_main_new (FALSE);
-
-    gpg_info.loop = loop;
-    gpg_info.verification = verification;
-
-    gpg_info.read_line_id =
-        g_signal_connect (line_buf,
-                          "read_line",
-                          (GCallback) gpg_read_line_cb,
-                          &gpg_info);
-    
-    gpg_info.read_done_id =
-        g_signal_connect (line_buf,
-                          "read_done",
-                          (GCallback) gpg_read_done_cb,
-                          &gpg_info);
-
-    g_main_run (loop);
-
-    g_object_unref (line_buf);
-
-    g_main_destroy (loop);
-
-    close (fds[0]);
-
-    waitpid (child, &status, 0);
-
-    if (!(WIFEXITED (status)) || (WEXITSTATUS (status) > 2)) {
-        rc_debug (RC_DEBUG_LEVEL_ERROR, __FUNCTION__ \
-                  ": gpg exited abnormally\n");
-
-        verification->status = RC_VERIFICATION_STATUS_UNDEF;
-        verification->info = g_strdup ("gpg returned an unknown error code");
-
-        return (verification);
-    }
-
-    return (gpg_info.verification);
-} /* rc_verify_gpg */
+}
 
 RCVerification *
 rc_verify_md5 (gchar *filename, guint8 *md5)
@@ -326,7 +270,7 @@ rc_verify_md5 (gchar *filename, guint8 *md5)
     g_free (cmd5);
 
     return (verification);
-} /* rc_verify_md5 */
+}
 
 RCVerification *
 rc_verify_md5_string (gchar *filename, gchar *md5)
@@ -353,7 +297,7 @@ rc_verify_md5_string (gchar *filename, gchar *md5)
     g_free (cmd5);
 
     return (verification);
-} /* rc_verify_md5_string */
+}
 
 RCVerification *
 rc_verify_size (gchar *filename, guint32 size)
@@ -385,4 +329,4 @@ rc_verify_size (gchar *filename, guint32 size)
     }
 
     return (verification);
-} /* rc_verify_size */
+}
