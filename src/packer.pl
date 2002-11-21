a1f1e94745d826e8955c29957c460d90f43274fd
Joe Shaw <joe@ximian.com>
2002-11-21 16:25:28 +0000

    Use binmode so perl 5.8.0 doesn't read in the data as UTF-8. Patch from
    2002-11-21  Joe Shaw  <joe@ximian.com>
    
    	* packer.pl: Use binmode so perl 5.8.0 doesn't read in the data
    	as UTF-8.  Patch from Michael Doppler.

--- src/packer.pl
+++ src/packer.pl
@@ -5,15 +5,24 @@ $name =~ s/[{}]//g;
 $name =~ s/[-\.]/_/g;
 
 open FOO, "$ARGV[0]";
+binmode FOO;
 
 $total = 0;
 
 print "static const char ", $name, "[] = { ";
 
 while ($len = read (FOO, $buf, 100)) {
+    if($total != 0){
+	printf(",");
+    }
     $total += $len;
-    foreach $char (split //, $buf) {
-        printf ("0x%X, ", ord ($char));
+    @list = (split //, $buf);
+    for($i = 0; $i < @list; ++$i) {
+        printf ("0x%X", ord ($list[$i]));
+	if($i+1 != @list) {
+		printf(",");
+	}
+	printf(" ");
     }
 }
 
