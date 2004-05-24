1770f9a5bcdaa7b0a2d5cc604fff637cd5eaee89
Vladimir Vukicevic <vladimir@src.gnome.org>
2004-05-24 00:05:30 +0000

    Added 4.3 stub (rpm 4.3 .so links against the right libs, so maybe we can
    * Makefile.am: Added 4.3 stub (rpm 4.3 .so links against the
    right libs, so maybe we can dispense with the stub, but it's
    easier to just keep it there)
    
    * rc-rpmman.c bumped LATEST_SUPPORTED_RPM_VERSION to 40301.
    (load_fake_syms): the hdrVec entries for 4.1-4.2.2 are valid for 4.3.1
    (load_rpm_syms): the hdrVec entries for 4.1-4.2.2 are valid for 4.3.1
    
    * distributions.xml: added fc2

--- src/rc-rpmman.c
+++ src/rc-rpmman.c
@@ -54,7 +54,7 @@
  * and have changed cordering from version to version.  It could be
  * very dangerous to call the wrong function.
  */
-#define LATEST_SUPPORTED_RPM_VERSION 40202
+#define LATEST_SUPPORTED_RPM_VERSION 40301
 
 #define GTKFLUSH {while (g_main_pending ()) g_main_iteration (TRUE);}
 
@@ -3023,7 +3023,7 @@ load_fake_syms (RCRpmman *rpmman)
     rpmman->rc_fdClose = &fdClose;
     /* Look for hdrVec in load_rpm_syms for an explanation of what's
      * going on */
-#if RPM_VERSION >= 40100 && RPM_VERSION <= 40202 /* RPM 4.1 to 4.2.2 inclusive */
+#if RPM_VERSION >= 40100 && RPM_VERSION <= 40301 /* RPM 4.1 to 4.3.1 inclusive */
     /* FIXME: untested */
     rpmman->headerGetEntry = ((void **)hdrVec)[16];
     rpmman->headerFree = ((void **)hdrVec)[2];
@@ -3172,7 +3172,7 @@ load_rpm_syms (RCRpmman *rpmman)
             rpmman->headerLoad = *(*hdrfuncs + 9);
         }
         else if (rpmman->version >= 40100 &&
-                 rpmman->version <= 40202) { /* RPM 4.1-4.2.2 inclusive */
+                 rpmman->version <= 40301) { /* RPM 4.1-4.3.1 inclusive */
             rpmman->headerFree = *(*hdrfuncs + 2);
             rpmman->headerGetEntry = *(*hdrfuncs + 16);
             rpmman->headerSizeof = *(*hdrfuncs + 6);
@@ -3516,6 +3516,9 @@ write_objects (void)
         } ObjectInfo;
 
         ObjectInfo objects[] = {
+            { "rc-{rpm_rpmio_rpmdb}-4.3.so",
+              rc_rpm_rpmio_rpmdb_4_3_so,
+              rc_rpm_rpmio_rpmdb_4_3_so_len },
             { "rc-{rpm_rpmio_rpmdb}-4.2.so",
               rc_rpm_rpmio_rpmdb_4_2_so,
               rc_rpm_rpmio_rpmdb_4_2_so_len },
@@ -3594,6 +3597,7 @@ rc_rpmman_init (RCRpmman *obj)
     gchar *so_file;
     char *object_dir;
     const char *objects[] = {
+        "rc-{rpm_rpmio_rpmdb}-4.3.so",
         "rc-{rpm_rpmio_rpmdb}-4.2.so",
         "rc-{rpm_rpmio_rpmdb}-4.1-popt.so.1.so",
         "rc-{rpm_rpmio_rpmdb}-4.1-popt.so.0.so",
