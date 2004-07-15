903690bb7ace790a1d0cfb20347b9b38d14944e1
James Willcox <james@ximian.com>
2004-07-15 01:10:05 +0000

    Import into GNOME CVS.
    2004-07-14  James Willcox  <james@ximian.com>
    
            * Import into GNOME CVS.

---
+++ bindings/mono/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+//
+// AssemblyInfo.cs
+//
+// Author:
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// (C) 2003 Ximian, Inc.  http://www.ximian.com
+//
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about the System.Web assembly
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyTitle("libredcarpet-sharp.dll")]
+[assembly: AssemblyDescription("libredcarpet-sharp.dll")]
+[assembly: AssemblyConfiguration("Development version")]
+[assembly: AssemblyCompany("Novell, Inc.")]
+[assembly: AssemblyProduct("libredcarpet")]
+[assembly: AssemblyCopyright("(c) 2004 Novell, Inc.")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyDefaultAlias("libredcarpet-sharp.dll")]
+[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AllowPartiallyTrustedCallers()]
+[assembly: AssemblyDelaySign(true)]
+[assembly: AssemblyKeyFile("ecma.pub")]
---
+++ bindings/mono/ServiceMountException.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace RC {
+	public class ServiceMountException : Exception {
+		public ServiceMountException (string msg) : base (msg) {}
+	}
+}
