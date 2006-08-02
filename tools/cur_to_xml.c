b9167d76789ae18c5c3553147f2c6b057a9e42d3
Tambet Ingo <tambet@ximian.com>
2006-08-02 14:26:48 +0000

    Fix to use the new rc_packman_query_all.
    2006-08-02  Tambet Ingo  <tambet@ximian.com>
    
    	* cur_to_xml.c (main): Fix to use the new rc_packman_query_all.

--- tools/cur_to_xml.c
+++ tools/cur_to_xml.c
@@ -29,68 +29,78 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    RCPackman *packman;
+    xmlNode *parent;
+    gboolean dump_files;
+} QueryAllInfo;
+
+static gboolean
+query_all_cb (RCPackage *package, gpointer user_data)
+{
+    RCPackageUpdate *pkg_up;
+    xmlNode *package_node;
+    QueryAllInfo *info = user_data;
+
+    pkg_up = rc_package_update_new ();
+
+    rc_package_spec_copy ((RCPackageSpec *)pkg_up,
+                          (RCPackageSpec *)package);
+
+    pkg_up->package_url = g_strdup ("file://dummy.rpm");
+    pkg_up->importance = RC_IMPORTANCE_SUGGESTED;
+    pkg_up->description = g_strdup ("No information available.");
+    pkg_up->package_size = 0;
+
+    package->history = g_slist_append (package->history, pkg_up);
+
+    package_node = rc_package_to_xml_node (package);
+
+    if (info->dump_files) {
+        RCPackageFileSList *files;
+        xmlNode *files_node;
+
+        files = rc_packman_file_list (info->packman, package);
+        files_node = rc_package_file_list_to_xml_node (files);
+        xmlAddChild (package_node, files_node);
+    }
+
+    xmlAddChild (info->parent, package_node);
+
+    return TRUE;
+}
+
 int
 main (int argc, char **argv)
 {
-    RCPackman *packman;
-    RCPackageSList *packages = NULL;
-    RCPackageSList *iter;
-    RCPackageUpdate *pkg_up;
     xmlDocPtr doc;
     xmlNode *channel_node;
     xmlNode *subchannel_node;
-    gboolean dump_files = FALSE;
+    QueryAllInfo info;
 
     if (argc == 2 && !strncmp ("--dump-files", argv[1], 12))
-        dump_files = TRUE;
+        info.dump_files = TRUE;
+    else
+        info.dump_files = FALSE;
 
     g_type_init ();
 
-    packman = rc_distman_new ();
+    info.packman = rc_distman_new ();
 
-    if (!packman)
+    if (!info.packman)
         g_error ("Couldn't access the packaging system");
 
-    if (rc_packman_get_error (packman)) {
+    if (rc_packman_get_error (info.packman)) {
         g_error ("Couldn't access the packaging system: %s",
-                 rc_packman_get_reason (packman));
+                 rc_packman_get_reason (info.packman));
     }
-
-    packages = rc_packman_query_all (packman);
 
     channel_node = xmlNewNode (NULL, "channel");
     subchannel_node = xmlNewNode (NULL, "subchannel");
     xmlAddChild (channel_node, subchannel_node);
 
-    for (iter = packages; iter; iter = iter->next) {
-        RCPackage *package = (RCPackage *)(iter->data);
-        xmlNode *package_node;
-
-        pkg_up = rc_package_update_new ();
-
-        rc_package_spec_copy ((RCPackageSpec *)pkg_up,
-                              (RCPackageSpec *)package);
-
-        pkg_up->package_url = g_strdup ("file://dummy.rpm");
-        pkg_up->importance = RC_IMPORTANCE_SUGGESTED;
-        pkg_up->description = g_strdup ("No information available.");
-        pkg_up->package_size = 0;
-
-        package->history = g_slist_append (package->history, pkg_up);
-
-        package_node = rc_package_to_xml_node (package);
-
-        if (dump_files) {
-            RCPackageFileSList *files;
-            xmlNode *files_node;
-
-            files = rc_packman_file_list (packman, package);
-            files_node = rc_package_file_list_to_xml_node (files);
-            xmlAddChild (package_node, files_node);
-        }
-
-        xmlAddChild (subchannel_node, package_node);
-    }
+    info.parent = subchannel_node;
+    rc_packman_query_all (info.packman, query_all_cb, &info);
 
     doc = xmlNewDoc ("1.0");
     xmlDocSetRootElement (doc, channel_node);
@@ -98,4 +108,3 @@ main (int argc, char **argv)
 
     exit (0);
 }
-    
